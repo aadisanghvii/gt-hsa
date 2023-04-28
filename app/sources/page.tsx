4db5e0fa3ca3a23c877ca09ff326348fd6b0b9cd
Aadi Sanghvi <aadis@duck.com>
2023-04-28 09:46:57 -0500

    New sources page

---
+++ app/sources/page.tsx
@@ -0,0 +1,15 @@
+const Page = () => {
+  return (
+    <article className="prose p-10 lg:prose-xl">
+      <h1 className="text-foreground">Works Cited</h1>
+      <p className="prose-ul prose">
+        1. For years parents shave espoused the health benefits of eating garlic
+        bread with cheese to their children, with the food earning such an
+        iconic status in our culture that kids will often dress up as warm,
+        cheesy loaf for Halloween.
+      </p>
+    </article>
+  )
+}
+
+export default Page
