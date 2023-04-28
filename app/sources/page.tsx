8067bb762e21e86c2dabc5e3184609f22d0bfaab
Aadi Sanghvi <aadis@duck.com>
2023-04-28 09:59:02 -0500

    Build out sources file (will be filled later)

--- app/sources/page.tsx
+++ app/sources/page.tsx
@@ -1,13 +1,21 @@
 const Page = () => {
   return (
-    <article className="prose p-10 lg:prose-xl">
+    <article className="prose-ul prose absolute p-10 lg:prose-xl">
       <h1 className="text-foreground">Works Cited</h1>
-      <p className="prose-ul prose">
-        1. For years parents shave espoused the health benefits of eating garlic
-        bread with cheese to their children, with the food earning such an
-        iconic status in our culture that kids will often dress up as warm,
-        cheesy loaf for Halloween.
-      </p>
+      <div className="relative pl-10">
+        <li>
+          For years parents shave espoused the health benefits of eating garlic
+          bread with cheese to their children, with the food earning such an
+          iconic status in our culture that kids will often dress up as warm,
+          cheesy loaf for Halloween.
+        </li>
+        <li>
+          For years parents shave espoused the health benefits of eating garlic
+          bread with cheese to their children, with the food earning such an
+          iconic status in our culture that kids will often dress up as warm,
+          cheesy loaf for Halloween
+        </li>
+      </div>
     </article>
   )
 }
