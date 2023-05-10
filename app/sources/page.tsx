cc3109c73591a20f02dc8faf8dac66315b945bfd
Aadi Sanghvi <aadis@duck.com>
2023-05-09 21:13:45 -0500

    refactor: sources appearance

--- app/sources/page.tsx
+++ app/sources/page.tsx
@@ -2,42 +2,90 @@ const Page = () => {
   return (
     <article className="prose-ul prose absolute p-10 lg:prose-xl">
       <h1 className="text-foreground">Works Cited:</h1>
-      <h3 className="text-foreground">Info</h3>
+      <h3 className="text-foreground">Technology</h3>
       <ol className="relative pl-10">
-        <li id="first-citation">
+        <li id="t1">
           For years parents shave espoused the health benefits of eating garlic
           bread with cheese to their children, with the food earning such an
           iconic status in our culture that kids will often dress up as warm,
           cheesy loaf for Halloween.
         </li>
-        <li id="second-citation">
-          For years parents shave espoused the health benefits of eating garlic
-          bread with cheese to their children, with the food earning such an
-          iconic status in our culture that kids will often dress up as warm,
-          cheesy loaf for Halloween
-        </li>
-        <li id="third-citation">
+        <li id="t2">
           For years parents shave espoused the health benefits of eating garlic
           bread with cheese to their children, with the food earning such an
           iconic status in our culture that kids will often dress up as warm,
           cheesy loaf for Halloween
         </li>
       </ol>
-      <h3 className="text-foreground">CRISPR-Cas9</h3>
+      <h3 className="text-foreground">Ethics</h3>
       <ol className="relative pl-10">
-        <li id="fourth-citation">
+        <li id="e1">
+          For years parents shave espoused the health benefits of eating garlic
+          bread with cheese to their children, with the food earning such an
+          iconic status in our culture that kids will often dress up as warm,
+          cheesy loaf for Halloween.
+        </li>
+        <li id="e2">
           For years parents shave espoused the health benefits of eating garlic
           bread with cheese to their children, with the food earning such an
           iconic status in our culture that kids will often dress up as warm,
           cheesy loaf for Halloween
         </li>
-        <li id="fifth-citation">
+      </ol>
+      <h3 className="text-foreground">Applications</h3>
+      <ol className="relative pl-10">
+        <li id="a1">
+          For years parents shave espoused the health benefits of eating garlic
+          bread with cheese to their children, with the food earning such an
+          iconic status in our culture that kids will often dress up as warm,
+          cheesy loaf for Halloween.
+        </li>
+        <li id="a2">
           For years parents shave espoused the health benefits of eating garlic
           bread with cheese to their children, with the food earning such an
           iconic status in our culture that kids will often dress up as warm,
           cheesy loaf for Halloween
         </li>
-        <li id="sixth-citation">
+      </ol>
+      <h3 className="text-foreground">History</h3>
+      <ol className="relative pl-10">
+        <li id="hi1">
+          For years parents shave espoused the health benefits of eating garlic
+          bread with cheese to their children, with the food earning such an
+          iconic status in our culture that kids will often dress up as warm,
+          cheesy loaf for Halloween.
+        </li>
+        <li id="hi2">
+          For years parents shave espoused the health benefits of eating garlic
+          bread with cheese to their children, with the food earning such an
+          iconic status in our culture that kids will often dress up as warm,
+          cheesy loaf for Halloween
+        </li>
+      </ol>
+      <h3 className="text-foreground">Future</h3>
+      <ol className="relative pl-10">
+        <li id="f1">
+          For years parents shave espoused the health benefits of eating garlic
+          bread with cheese to their children, with the food earning such an
+          iconic status in our culture that kids will often dress up as warm,
+          cheesy loaf for Halloween.
+        </li>
+        <li id="f2">
+          For years parents shave espoused the health benefits of eating garlic
+          bread with cheese to their children, with the food earning such an
+          iconic status in our culture that kids will often dress up as warm,
+          cheesy loaf for Halloween
+        </li>
+      </ol>
+      <h3 className="text-foreground">Science</h3>
+      <ol className="relative pl-10">
+        <li id="s1">
+          For years parents shave espoused the health benefits of eating garlic
+          bread with cheese to their children, with the food earning such an
+          iconic status in our culture that kids will often dress up as warm,
+          cheesy loaf for Halloween.
+        </li>
+        <li id="s2">
           For years parents shave espoused the health benefits of eating garlic
           bread with cheese to their children, with the food earning such an
           iconic status in our culture that kids will often dress up as warm,
